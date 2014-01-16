75389329072a7d7c31bee5f2cadb57c31fb9c674
resag <sagalynr@onid.orst.edu>
2014-01-16 18:59:06 -0500

    cleanup config files

--- config/db.php
+++ config/db.php
@@ -1,9 +1,23 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "root";	
-$db_pass = "root";	
-$db_name = "db_users";	
+
+if ( $_SERVER['DOCUMENT_ROOT'] == '/home/rebecc82/public_html' ) { // if live
+
+	$db_host = "localhost";
+	$db_user = "rebecc82_admin";	
+	$db_pass = 'S4Db?sa;o(Lo';
+	$db_name = "rebecc82_db_users";	
+
+} elseif ( $_SERVER['DOCUMENT_ROOT'] == '/Users/rsagalyn/projects/phplogin' ) { // if local
+
+	$db_host = "localhost";
+	$db_user = "root";	
+	$db_pass = "root";	
+	$db_name = "db_users";	
+
+}
+
+
 
 
 // create a database connection
--- init.php
+++ init.php
@@ -1,18 +1,34 @@
 <?php
 
-// Do i need an ssl cert???
-//ini_set('session.cookie_secure',1);
-ini_set('session.cookie_httponly',1);
-ini_set('session.use_only_cookies',1);
+/* 
+ * should be included at the top of every page
+ * does the following: 
+ * 	- starts (resumes) a session
+ *  - defines database constants
+ * 	- opens a new connection to the database
+ * 	- checks the php version and includes the password compatibility library if necessary
+ * 	  or stops the script's execution if PHP < 5.3.7
+ *
+ */
 
-// start/resume session
+
+// Turn on all errors
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
+// help prevent session fixation
+ini_set('session.cookie_httponly',1);
+ini_set('session.use_only_cookies',1);	// don't store SIDs in URLs
+//ini_set('session.cookie_secure',1);	// turn this on if SSL cert
+
+
+// Start/resume session
 session_start();
 
-// Required configuration files go here
-require_once("config/config.php");
+// Required configuration files/libraries
 require_once("config/db.php");
 require_once("classes/user.php");
-
 
 // Check php version; password hashing compatibility library requires PHP >= 5.3.7
 // If PHP < 5.3.7, website won't work
@@ -20,9 +36,8 @@ if (version_compare(PHP_VERSION, '5.3.7', '<')) {
 	exit("PHP >= 5.3.7 is required");
 }
 
-// PHP hashing API requires PHP >= 5.5
-// If 5.3.7 <= PHP < 5.5, use password_compat library 
-// https://github.com/ircmaxell/password_compat
+// PHP password hashing API requires PHP >= 5.5
+// If 5.3.7 <= PHP < 5.5, use password_compat library (https://github.com/ircmaxell/password_compat)
 elseif (version_compare(PHP_VERSION, '5.5.0', '<')) {
 	require_once("libraries/password.php");
 }
